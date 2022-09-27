2b1bd689aff00746082430d6595cc0b1f7ab4988
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-09-27 13:33:20 +0600

    add product code added

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const addProduct = () => {
-  console.log("h");
+  const product = document.getElementById("input-field-key");
+  const quantity = document.getElementById("input-field-value");
+
+  addCartToLS(product.value, quantity.value);
+
+  product.value = "";
+  quantity.value = "";
+};
+
+const getStoredCart = () => {
+  let cart = {};
+
+  const storedCart = localStorage.getItem("cart");
+
+  if (storedCart) {
+    cart = JSON.parse(storedCart);
+  }
+  return cart;
+};
+
+const addCartToLS = (product, quantity) => {
+  const cart = getStoredCart();
+
+  cart[product] = quantity;
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  return cart;
 };
